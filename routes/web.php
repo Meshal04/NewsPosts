38e96b010d60b650608ab3175d6daef0c65e02d0
Meshal04 <meshal.04q@gmail.com>
2025-04-13 10:40:01 +0300

    debugged some issues
    13 april

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\role;
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,22 +22,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('comments', CommentController::class);
+    Route::resource('posts', PostController::class);
+    Route::resource('news', NewsController::class);
+    Route::resource('categories', CategoryController::class);
 });
-
-Route::resource('posts', PostController::class);
-Route::resource('news', NewsController::class);
-Route::resource('categories', CategoryController::class);
-
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::resource('posts', PostController::class)->except(['index', 'show']);
-    Route::resource('news', NewsController::class)->except(['index', 'show']);
-    Route::resource('categories', CategoryController::class)->except(['index', 'show']);
-});
-
-// Route::middleware(['auth', 'role:writer'])->group(function () {
-//     Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
-//     Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-//     Route::get('news/create', [NewsController::class, 'create'])->name('news.create');
-//     Route::post('news', [NewsController::class, 'store'])->name('news.store');
-// });
-
